5585382405b3b9b70c230d0bc0651b2b0760ac53
mhzin <95629887+mhzin-dev@users.noreply.github.com>
2022-11-10 20:55:33 -0300

    up

---
+++ Jokenpo/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jokenpo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] jokenpo = { "Pedra", "Papel", "Tesoura" };
+
+            Random r = new Random();
+
+            int posicao = r.Next(jokenpo.Length);
+
+            string rnd_play = jokenpo[posicao];
+
+            Console.WriteLine("Pedra \nPapel \nTesoura\n\nQual será sua jogada? ");
+            string player = Console.ReadLine();
+
+            if (rnd_play == player)
+            {
+                Console.WriteLine("\nSua jogada: " + player);
+                Console.WriteLine("\nJogada do Computador: " + rnd_play + "\n");
+                Console.WriteLine("Empate!");
+            }
+            else
+            {
+                if (rnd_play == "Pedra" && player == "Tesoura")
+                {
+                    Console.WriteLine("\nSua jogada: " + player);
+                    Console.WriteLine("\nJogada do Computador: " + rnd_play + "\n");
+                    Console.WriteLine("A maquina ganhou!");
+                }
+                else if (rnd_play == "Papel" && player == "Pedra")
+                {
+                    Console.WriteLine("\nSua jogada: " + player);
+                    Console.WriteLine("\nJogada do Computador: " + rnd_play + "\n");
+                    Console.WriteLine("A maquina ganhou!");
+                }
+                else if (rnd_play == "Tesoura" && player == "Papel")
+                {
+                    Console.WriteLine("\nSua jogada: " + player);
+                    Console.WriteLine("\nJogada do Computador: " + rnd_play + "\n");
+                    Console.WriteLine("A maquina ganhou!");
+                }
+                else
+                {
+                    Console.WriteLine("\nSua jogada: " + player);
+                    Console.WriteLine("\nJogada do Computador: " + rnd_play + "\n");
+                    Console.WriteLine("Você ganhou!");
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+## Bhaskara Calculator
+
+```p
+ - Script Basico de Pedra, papel e tesoura ou Jokenpo. Feito em C#
+```
+
+<img src="./assets/img_1.png">
+<img src="./assets/img_2.png">
+
+
+---
+
+- <p align="center"> Hi, I’m @mhzin
+
+---
